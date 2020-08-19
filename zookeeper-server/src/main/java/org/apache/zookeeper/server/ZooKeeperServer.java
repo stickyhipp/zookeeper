a021781a02cb2f8b5663a63e13e7a04835a3c1b9
Peter Welch <peter.welch@gmail.com>
2020-08-19 15:57:01 -0700

    Revert "ZOOKEEPER-3415: convert internal logic to use java8 streams"
    This reverts commit 1bd663a23d285f11435ad7f6df7022548346f4fb.

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooKeeperServer.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooKeeperServer.java
@@ -498,9 +498,17 @@ public class ZooKeeperServer implements SessionExpirer, ServerStats.Provider {
         }
 
         // Clean up dead sessions
-        zkDb.getSessions().stream()
-                        .filter(session -> zkDb.getSessionWithTimeOuts().get(session) == null)
-                        .forEach(session -> killSession(session, zkDb.getDataTreeLastProcessedZxid()));
+        List<Long> deadSessions = new ArrayList<>();
+        for (Long session : zkDb.getSessions()) {
+            if (zkDb.getSessionWithTimeOuts().get(session) == null) {
+                deadSessions.add(session);
+            }
+        }
+
+        for (long session : deadSessions) {
+            // TODO: Is lastProcessedZxid really the best thing to use?
+            killSession(session, zkDb.getDataTreeLastProcessedZxid());
+        }
 
         // Make a clean snapshot
         takeSnapshot();
@@ -1640,7 +1648,12 @@ public class ZooKeeperServer implements SessionExpirer, ServerStats.Provider {
     }
 
     private boolean hasCnxSASLAuthenticated(ServerCnxn cnxn) {
-        return cnxn.getAuthInfo().stream().anyMatch(id -> id.getScheme().equals(SASL_AUTH_SCHEME));
+        for (Id id : cnxn.getAuthInfo()) {
+            if (id.getScheme().equals(SASL_AUTH_SCHEME)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void processSasl(ByteBuffer incomingBuffer, ServerCnxn cnxn, RequestHeader requestHeader) throws IOException {
