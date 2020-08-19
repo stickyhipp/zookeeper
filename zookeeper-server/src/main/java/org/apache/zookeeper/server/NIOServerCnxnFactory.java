9589c0f7a47095b10f8cdcc29fd26bd1428ec0ac
Peter Welch <peter.welch@gmail.com>
2020-08-19 15:57:01 -0700

    Revert "ZOOKEEPER-3883: new UncaughtExceptionHandler object with lambda"
    This reverts commit 58dc1207d12706be913a43103c6cd912142d51cf.

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/NIOServerCnxnFactory.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/NIOServerCnxnFactory.java
@@ -82,7 +82,11 @@ public class NIOServerCnxnFactory extends ServerCnxnFactory {
     public static final String ZOOKEEPER_NIO_SHUTDOWN_TIMEOUT = "zookeeper.nio.shutdownTimeout";
 
     static {
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> LOG.error("Thread {} died", t, e));
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            public void uncaughtException(Thread t, Throwable e) {
+                LOG.error("Thread {} died", t, e);
+            }
+        });
 
         /**
          * Value of 0 disables use of direct buffers and instead uses
